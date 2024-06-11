b94920365b048f06f8b976d9b27724be1643de91
vivi <00001117011860sp@al.educacao.sp.gov.br>
2024-06-11 15:53:58 -0300

    adicionando arquivos

--- sketch.js
+++ sketch.js
@@ -1,1 +1,33 @@
+let palavra;
+
+function setup() {
+  createCanvas(400, 400);
+
+  palavra = palavraAleatoria();
+}
+
+function palavraAleatoria() {
+  let palavras = ["butterfly", "butter", "beautiful"];
+  return random(palavras);
+}
+
+function inicializaCores() {
+  background("pink");
+  fill("yellow");
+  textSize(64);
+  textAlign(CENTER, CENTER);
+}
+
+function draw() {
+  inicializaCores();
+
+  let maximo = width;
+  let minimo = 0;
+  // mouseX, 0, width ==> 0, palavra.length
+
+  let quantidade = map(mouseX, 0, width, 1, palavra.length);
+  //console.log(quantidade);
+  let parcial = palavra.substring(0, quantidade);
+  text(parcial, 200, 200);
+}
 
